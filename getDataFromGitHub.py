cc82e58d267ddb1ed145822a02574e985f657aab
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2021-06-21 09:44:00 +0700

    Updated to work with the scope of the study.

--- getDataFromGitHub.py
+++ getDataFromGitHub.py
@@ -28,13 +28,14 @@ import math
 #############
 
 URL = "https://api.github.com/search/repositories?q="  # The basic URL to use the GitHub API
-QUERY = "user:rsain"  # The personalized query (for instance, to get repositories from user 'rsain')
-SUB_QUERIES = ["+created%3A<%3D2021-03-31",
-              "+created%3A>%3D2014-01-01"]  # Different sub-queries if you need to collect more than 1000 elements
+# The personalized query (for instance, to get repositories from user 'rsain')
+QUERY = "topic:machine-learning"
+# Different sub-queries if you need to collect more than 1000 elements
+SUB_QUERIES = ["+created%3A<%3D2021-05-31+created%3A>%3D2014-01-01"]
 PARAMETERS = "&per_page=100"  # Additional parameters for the query (by default 100 items per page)
 DELAY_BETWEEN_QUERIES = 10  # The time to wait between different queries to GitHub (to avoid be banned)
-OUTPUT_FOLDER = "/Users/chaiyong/Desktop/GitHub-Crawler/"  # Folder where ZIP files will be stored
-OUTPUT_CSV_FILE = "/Users/chaiyong/Desktop/GitHub-Crawler/repositories.csv"  # Path to the CSV file generated as output
+OUTPUT_FOLDER = "/Users/chaiyong/Docs/Teaching/2021/GitHub-Crawler/downloads/"  # Folder where ZIP files will be stored
+OUTPUT_CSV_FILE = "/Users/chaiyong/Docs/Teaching/2021/GitHub-Crawler/repositories.csv"  # Path to the CSV file generated as output
 
 
 #############
@@ -46,13 +47,13 @@ def getUrl(url):
     response = requests.get(url)
     return response.json()
 
-
 ########
 # MAIN #
 ########
 
+
 # To save the number of repositories processed
-countOfRepositories = 0
+count_of_repos = 0
 
 # Output CSV file which will contain information about repositories
 csv_file = open(OUTPUT_CSV_FILE, 'w')
@@ -63,34 +64,42 @@ for subquery in range(1, len(SUB_QUERIES) + 1):
     print("Processing subquery " + str(subquery) + " of " + str(len(SUB_QUERIES)) + " ...")
     # Obtain the number of pages for the current subquery (by default each page contains 100 items)
     url = URL + QUERY + str(SUB_QUERIES[subquery - 1]) + PARAMETERS
+    print(url)
     data = json.loads(json.dumps(getUrl(url)))
     numberOfPages = int(math.ceil(data['total_count'] / 100.0))
-    print("No. of pages = " + numberOfPages)
+    print("No. of pages = " + str(numberOfPages))
 
     # Results are in different pages
     for currentPage in range(1, numberOfPages + 1):
         print("Processing page " + str(currentPage) + " of " + str(numberOfPages) + " ...")
         url = URL + QUERY + str(SUB_QUERIES[subquery - 1]) + PARAMETERS + "&page=" + str(currentPage)
+        print(url)
+
         data = json.loads(json.dumps(getUrl(url)))
         # Iteration over all the repositories in the current json content page
-        for item in data['items']:
-            # Obtain user and repository names
-            user = item['owner']['login']
-            repository = item['name']
-            repositories.writerow([user, repository])
-            # Download the zip file of the current project
-            print("Downloading repository '%s' from user '%s' ..." % (repository, user))
-            url = item['clone_url']
-            fileToDownload = url[0:len(url) - 4] + "/archive/master.zip"
-            fileName = item['full_name'].replace("/", "#") + ".zip"
-            wget.download(fileToDownload, out=OUTPUT_FOLDER + fileName)
-            # Update repositories counter
-            countOfRepositories = countOfRepositories + 1
+        try:
+            for item in data['items']:
+                # Obtain user and repository names
+                user = item['owner']['login']
+                repository = item['name']
+                clone_url = item['clone_url']
+                repositories.writerow([user, repository, clone_url])
 
+        #         # # Download the zip file of the current project
+        #         # print("Downloading repository '%s' from user '%s' ..." % (repository, user))
+        #         # url = item['clone_url']
+        #         # fileToDownload = url[0:len(url) - 4] + "/archive/master.zip"
+        #         # fileName = item['full_name'].replace("/", "#") + ".zip"
+        #         # wget.download(fileToDownload, out=OUTPUT_FOLDER + fileName)
+        #
+        #         # Update repositories counter
+        #         count_of_repos = count_of_repos + 1
+        except:
+            print("Error: " + user + ", " + repository + "," + clone_url)
     # A delay between different sub-queries
     if subquery < len(SUB_QUERIES):
         print("Sleeping " + str(DELAY_BETWEEN_QUERIES) + " seconds before the new query ...")
-        time.sleep(DELAY_BETWEEN_QUERIES)
+        # time.sleep(DELAY_BETWEEN_QUERIES)
 
-print("DONE! " + str(countOfRepositories) + " repositories have been processed.")
+print("DONE! " + str(count_of_repos) + " repositories have been processed.")
 csv_file.close()
